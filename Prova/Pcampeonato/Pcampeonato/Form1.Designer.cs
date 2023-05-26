bdd9db218b235de0c271d6bf116d21725f181e49
Gabriel Telo Mariano <116767607+GabrielTeloMariano@users.noreply.github.com>
2023-05-26 20:21:54 -0300

    Add files via upload

---
+++ Prova/Pcampeonato/Pcampeonato/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Pcampeonato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.lstbxLista = new System.Windows.Forms.ListBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(15, 15);
+            this.btnExecutar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(146, 126);
+            this.btnExecutar.TabIndex = 0;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.BtnExecutar_Click);
+            // 
+            // lstbxLista
+            // 
+            this.lstbxLista.FormattingEnabled = true;
+            this.lstbxLista.ItemHeight = 24;
+            this.lstbxLista.Location = new System.Drawing.Point(170, 15);
+            this.lstbxLista.Name = "lstbxLista";
+            this.lstbxLista.Size = new System.Drawing.Size(592, 292);
+            this.lstbxLista.TabIndex = 1;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(655, 381);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(107, 43);
+            this.btnFechar.TabIndex = 2;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.BtnFechar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(532, 381);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(107, 43);
+            this.btnLimpar.TabIndex = 3;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 436);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.lstbxLista);
+            this.Controls.Add(this.btnExecutar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.ListBox lstbxLista;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Prova/Pcampeonato/Pcampeonato/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Windows.Forms;
+
+
+
+namespace Pcampeonato
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            lstbxLista.Items.Clear();
+        }
+
+        private void BtnExecutar_Click(object sender, EventArgs e)
+        {
+            int[,] matInt = new int[4, 3];
+            string texto = "";
+            int totalFeitos = 0, totalRecebidos = 0;
+
+            for (var j = 0; j < 4; j++)
+            {
+                int k = 0;
+
+                var inputFeitos = Interaction.InputBox($"Gols feitos pelo time {j + 1}: ", "Entrada de dados");
+                if (!int.TryParse(inputFeitos, out matInt[j, 0]))
+                {
+                    MessageBox.Show("Favor inserir um número inteiro");
+                    j--;
+                    continue;
+                }
+
+                totalFeitos += matInt[j, 0];
+
+                var inputRecebidos = "";
+                while (k == 0)
+                {
+                    inputRecebidos = Interaction.InputBox($"Gols recebidos pelo time {j + 1}: ", "Entrada de dados");
+                    if (!int.TryParse(inputRecebidos, out matInt[j, 1]))
+                    {
+                        MessageBox.Show("Favor inserir um número inteiro");
+                        continue;
+                    }
+                    else
+                    {
+                        k = 1;
+                    }
+                }
+                totalRecebidos += matInt[j, 1];
+
+
+                matInt[j, 2] = matInt[j, 0] - matInt[j, 1];
+                texto = $"Time {j + 1} Gols feitos: {matInt[j, 0]}  Gols recebidos: {matInt[j, 1]}  Saldo de Gols: {matInt[j, 2]}";
+                lstbxLista.Items.Add(texto);
+            }
+
+            lstbxLista.Items.Add("\n --------------------------------\n");
+            lstbxLista.Items.Add($"Total gols feitos: {totalFeitos}\n");
+            lstbxLista.Items.Add($"Total gols recebidos: {totalRecebidos}");
+
+        }
+    }
+}
+
